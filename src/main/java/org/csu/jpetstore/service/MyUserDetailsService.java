148e0a0959c3784106078376e44a82669948d7f1
Chun Yu <junyussh@gmail.com>
2020-06-13 05:52:34 +0800

    fix: not exist username login error is not same with exist username's error

--- src/main/java/org/csu/jpetstore/service/MyUserDetailsService.java
+++ src/main/java/org/csu/jpetstore/service/MyUserDetailsService.java
@@ -1,7 +1,9 @@
 package org.csu.jpetstore.service;
 
 import org.csu.jpetstore.bean.Account;
+import org.csu.jpetstore.exception.ApiRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,9 +16,13 @@ public class MyUserDetailsService implements UserDetailsService {
     private AccountService accountService;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println(username);
         Account account = accountService.selectAccountByID(username);
         if (account == null) {
             account = accountService.selectAccountByUsername(username);
+        }
+        if (account == null) {
+            throw new ApiRequestException("Invalid username or password!", HttpStatus.UNAUTHORIZED);
         }
         UserDetails userDetails = User.builder()
                 .username(account.getId().toString())
